44f73b5f4c95d6159bc76d21c5cd7f6761ef042b
Pirris <luissalas_10@outlook.com>
2020-07-24 18:30:59 -0600

    Código utilizando estructuras Map

---
+++ example/MapsEnDart.dart
@@ -0,0 +1,37 @@
+import 'dart:io';
+import 'package:test/test.dart';
+
+ main(List<String> arguments){
+
+   /**
+    * En esta clase aprenderemos acerca de map
+    * Básicamente se les un identificador numeral a las variable o valores
+    */
+
+   Map <int,String> map; //este es un map tipado, que acepta enteros como identificadores y cadenas como valores
+  map = {1:'Rojo', 2:'Verde',3:'Amarillo'};
+  print(map);
+
+  map[4]= 'Morado'; // Así se agregan valores nuevos al map, declarando el identificador y el valor
+
+
+
+   // También está esta forma de declarar maps, maps no tipados
+   //Por ende acepta cuallquier tipo de dato, ya sea en el identificador numeral o en el campo del valor.
+   Map nombres;
+   nombres = {0: 'Luis', 1:'Fiorella', 2:'Isennya'};
+   print(nombres);
+
+   nombres[3]= 256;
+   print(nombres);
+
+   /**
+    * Finalmente, vimos un tipo de tipado utilizando el tipado dynamic para el campo de valor
+    */
+
+   Map provincias = <int,dynamic> {0:'San Jose', 1:'Guancaste', 2:'Cartago'};
+   print(provincias);
+
+   provincias[3] = 2584.23; // Notar que al asignarle un valor al identificador 3, el mismo puede ser un número.
+   print(provincias);
+ }
